4304eae94e4231a84ec876625e930e53a13ea187
Mikhail Grishkin <mishabigr@yandex.ru>
2021-09-22 20:03:01 +0300

    Add signup component

--- recordstore_front/src/router/index.js
+++ recordstore_front/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Signin from '@/components/Signin'
+import Signup from '@/components/Signup'
 // import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
@@ -9,9 +10,17 @@ export default new Router({
     mode: 'history',
     routes: [{
 
-        path: '/',
-        name: 'Signin',
-        component: Signin
+            path: '/',
+            name: 'Signin',
+            component: Signin
 
-    }]
+        },
+
+        {
+            path: '/signup',
+            name: 'Signup',
+            component: Signup
+
+        }
+    ]
 })
