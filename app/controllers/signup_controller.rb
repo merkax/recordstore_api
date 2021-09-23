c39a40a3e570f0e4b2f844158e6c80ef5f92167f
Mikhail Grishkin <mishabigr@yandex.ru>
2021-09-23 15:53:58 +0300

    Fix mistakes in sign_in sign_up controllers

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,7 +1,7 @@
 class SignupController < ApplicationController
   def create
     user = User.new(user_params)
-  
+
     if user.save
       payload = { user_id: user.id }
       session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
@@ -10,9 +10,9 @@ class SignupController < ApplicationController
       response.set_cookie(JWTSessions.access_cookie,
                           value: tokens[:access],
                           httponly: true,
-                          secure: Rails.env.production?)
-      
-      render json: { crsf: tokens[:crsf] }
+                          secure: Rails.env.production? )
+
+      render json: { csrf: tokens[:csrf] }
     else
       render json: { error: user.errors.full_messages.join(' ') }, status: :unprocessable_entity
     end
