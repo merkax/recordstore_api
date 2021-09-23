dfde8d7e852109e1cffbfb801bda68e27e8633e9
Mikhail Grishkin <mishabigr@yandex.ru>
2021-09-23 15:48:59 +0300

    Add artists and records vue router

--- recordstore_front/src/router/index.js
+++ recordstore_front/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Signin from '@/components/Signin'
 import Signup from '@/components/Signup'
+import Artists from '@/components/artists/Artists'
+import Records from '@/components/records/Records'
 // import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
@@ -9,6 +11,20 @@ Vue.use(Router)
 export default new Router({
     mode: 'history',
     routes: [{
+
+            path: '/artists',
+            name: 'Artist',
+            component: Artists
+
+        },
+        {
+
+            path: '/records',
+            name: 'Records',
+            component: Records
+
+        },
+        {
 
             path: '/',
             name: 'Signin',
